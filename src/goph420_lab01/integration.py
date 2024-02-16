55eb594664ab925e56d34e35ea3a4e2ef8eb7f91
Stephen Liu <stephen.liu@ucalgary.ca>
2024-02-16 14:40:31 -0700

    updated integration fnc and began testing

--- src/goph420_lab01/integration.py
+++ src/goph420_lab01/integration.py
@@ -13,24 +13,33 @@ def integrate_newton(x,f,alg="trap"):
     -------
     float
     """
+    x=np.array([x])
+    f=np.array([f])
     N=len(x)
     area=0
     intervals=N//2
-    sumx=np.ndarray([[]])
+    sumx=np.array([[]])
     if alg=="trap":
-        for i in range (0,N):
+        for i in range (0,N-1):
             area+=((x[i+1]-x[i])/2)*(f[i+1]+f[i])
+        return area
     elif alg=="simp":
-        if N>=3 and N//2==0:
-            pass
-        elif N>=3 and N//2!=0:
+        if N>=3 and N % 2==0:
+            for i in range (1,N-3,2):
+                sumx[0]+=f[i]
+            for i in range (2,N-4,2):
+                sumx[1]+=f[i]
+            area=1/3*intervals*(f[0]+4*sumx[0]+2*sumx[1]+f[N-1])+((x[N]-x[0])/8*(f[N-3]+3*f[N-2]+3*f[N-1]+f[N]))
+        elif N>=3 and N % 2!=0:
             for i in range (1,N,2):
                 sumx[0]+=f[i]
             for i in range (2,N-1,2):
                 sumx[1]+=f[i]
-            area=1/3*intervals*(f[0]+4*sumx[1]+2*sumx[2]+f[N])
-            #np.ndarray([[2*x[i]**2-3*x[i]+1, 4*x[i]-4*x[i]**2, 2*x[i]**2-x[i]]])
-
+            area=1/3*intervals*(f[0]+4*sumx[0]+2*sumx[1]+f[N-1])
+            #np.array([[2*x[i]**2-3*x[i]+1, 4*x[i]-4*x[i]**2, 2*x[i]**2-x[i]]])
+        return area
+    else:
+        raise ValueError ("Invalid algorithm entered, expected 'simp' or 'trap'")
 
 def integrate_gauss(f, lims, npts):
     """
--- tests/test_newton.py
+++ tests/test_newton.py
@@ -1,9 +1,17 @@
-from goph420_lab01.integration import (
-    integrate_newton,
-)
+# from goph420_lab01.integration import (
+#     integrate_newton,
+# )
 import numpy as np
 
-x=np.ndarray([[1,2,3,4]])
-f=np.ndarray([[2,3,4,5]])
+x=np.array([1,2,3,4,5,6,7])
+f=np.array([2,3,4,5,6,7,8])
+sumx=np.array([0,0])
+N=4
+intervals=N//2
 
-integrate_newton(x,f,alg="trap")
+if N>=3 and N % 2==0:
+    print ("1")
+elif N>=3 and N % 2!=0:
+    print ("2")
+
+# integrate_newton(x,f,alg="trap")
