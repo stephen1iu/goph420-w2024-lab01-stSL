61ebc8e94e414815be6b81fa84ceafe7afbfbc79
Stephen Liu <stephen.liu@ucalgary.ca>
2024-02-29 20:59:40 -0700

    added pyproject and requirements

--- tests/test_newton.py
+++ tests/test_newton.py
@@ -1,7 +1,7 @@
-from goph420_lab01.integration import (
-    integrate_newton,
-)
+from goph420_lab01.integration import integrate_newton
+
 import numpy as np
+
 #function of y=x+1
 x=np.array([1,2,3,4,5,6,7])
 f=np.array([2,3,4,5,6,7,8])
