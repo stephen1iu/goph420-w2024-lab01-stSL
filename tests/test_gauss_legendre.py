adefcac4334a63955f274571c9a231b6f1d68acb
Stephen Liu <stephen.liu@ucalgary.ca>
2024-03-06 14:15:54 -0700

    started work on testing gauss legendre

---
+++ tests/test_gauss_legendre.py
@@ -0,0 +1,24 @@
+from goph420_lab01.integration import integrate_gauss
+
+from scipy.integrate import fixed_quad
+import unittest
+import numpy as np
+
+
+class TestGaussLegendre(unittest.TestCase):
+    
+    def setUp(self):
+        lims=np.array([0,8])
+        self.f=
+
+
+    def test_npts1(self):
+        
+    def test_npts2(self):
+        pass
+    def test_npts3(self):
+        pass
+    def test_npts4(self):
+        pass
+    def test_npts5(self):
+        pass
